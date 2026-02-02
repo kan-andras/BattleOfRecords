56f1c00d630093b4f8d264e3474af44743c44341
kan-andras <talanegyszer987@gmail.com>
2026-02-02 13:00:40 +0100

    Karakter class változtatása. Adatbázis struktura kialakítás.

--- BattleOfRecords/Model/Karakterek.cs
+++ BattleOfRecords/Model/Karakterek.cs
@@ -8,19 +8,22 @@ namespace BattleOfRecords.Model
 {
     internal class Karakterek
     {
-        public int id { get; set; }
-        public string nev { get; set; }
+        public int _karakter_id
+        public int _jatek_id { get; set; }
+        public string karakterneve { get; set; }
         public int eletero { get; set; }
         public int mana { get; set; }
         public int tamadoero { get; set; }
 
-        public Karakterek(int id, string nev, int eletero, int mana, int tamadoero)
+        public Karakterek(int _karakter, int _jatek, int _mana, int _tamadoero, int _eletero, string _karakternev)
         {
-            this.id = id;
-            this.nev = nev;
-            this.eletero = eletero;
-            this.mana = mana;
-            this.tamadoero = tamadoero;
+            this.eletero = _eletero;
+            this.karakterneve = _karakternev;
+            this.mana = _mana;
+            this._karakter_id = _karakter;
+            this.tamadoero = _tamadoero;
+            this._jatek_id = _jatek;
         }
     }
+
 }
